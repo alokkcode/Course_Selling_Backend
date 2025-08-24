3ad2a2301ca425bc2ba1cd5a852cb0e60bc4e691
alokkcode <alokkumar.code@gmail.com>
2025-08-25 02:12:00 +0530

    Admin route skeleton added

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const express = require("express");
 const {userRouter} = require("./routes/user");
 const {courseRouter} = require("./routes/course");
+const { adminRouter } = require("./routes/admin");
+
 const jwt = require("jsonwebtoken");
 
 
@@ -11,6 +13,7 @@ const app = express();
 
 app.use("/user", userRouter);
 app.use("/course", courseRouter); 
+app.use("/admin", adminRouter); 
 
 
 app.listen(3000, () => {
---
+++ routes/admin.js
@@ -0,0 +1,41 @@
+const { Router } = require("express");
+const adminRouter = Router();
+
+
+adminRouter.post("/signup", function (req, res) {
+    res.json({
+        message: "Signup endpoint"
+    })
+})
+
+adminRouter.post("/signin", function (req, res) {
+    res.json({
+        message: "Signin endpoint"
+    })
+})
+
+
+adminRouter.post("/course", function (req, res) { // to create a course by admins
+    res.json({
+        message: "Course Created by Admin"
+    })
+})
+
+
+adminRouter.put("/course", function (req, res) { // to change say , name of course by admins
+    res.json({
+        message: "Course Name changed"
+    })
+})
+
+
+adminRouter.get("/course/bulk", function (req, res) { // admin can get all their courses
+    res.json({
+        message: "Course Created by Admin"
+    })
+})
+
+
+module.exports = {
+    adminRouter : adminRouter
+}
