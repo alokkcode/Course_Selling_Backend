5a967fc48619a3d579bd14c790f582961bef8b54
alokkcode <alokkumar.code@gmail.com>
2025-08-28 02:36:20 +0530

    Added signup route with validation

--- index.js
+++ index.js
@@ -6,11 +6,10 @@ const { userRouter } = require("./routes/user");
 const { courseRouter } = require("./routes/course");
 const { adminRouter } = require("./routes/admin");
 
-const jwt = require("jsonwebtoken");
 
 
 const app = express();
-
+app.use(express.json());
 
 app.use("/user", userRouter);
 app.use("/course", courseRouter);
--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,69 @@
 const { Router } = require("express");
 const userRouter = Router();
+const { z } = require("zod");
+const { userModel } = require("../db");
+const { JWT_USER_SECRET } = require("../config");
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken");
 
 
-userRouter.post("/signup", function (req, res) {
-    res.json({
-        message: "Signup endpoint"
+userRouter.post("/signup", async function (req, res) {
+    const requiredBody = z.object({ 
+        email: z.string().min(3).max(50).email(),
+        password: z.string().min(6).max(100).refine(function (val) {
+            return /[A-Z]/.test(val);
+        }, {
+            error: "Password must contain at least one uppercase character"
+        }).refine(function (val) {
+            return /[a-z]/.test(val);
+        }, {
+            error: "Password must contain at least one lowercase character"
+        }).refine(function (val) {
+            return /[^a-zA-Z0-9]/.test(val);
+        }, {
+            error: "Password must contain at least one special character"
+        }),
+
+        firstName: z.string(),
+        lastName: z.string()
     })
+
+    const parsedDataWithSuccess = requiredBody.safeParse(req.body);
+
+    if (!parsedDataWithSuccess.success) {
+        res.json({
+            message: "Incorrect Format",
+            error: parsedDataWithSuccess.error
+        })
+        return
+    }
+
+    const { email, password, firstName, lastName } = req.body;
+    let errorThrown = false;
+
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        await userModel.create({
+            email: email,
+            password: hashedPassword,
+            firstName: firstName,
+            lastName: lastName
+        })
+    } catch (e) {
+        res.json({
+            message: "User already exists"
+        })
+
+        errorThrown = true;
+    }
+
+    if (!errorThrown) {
+        res.json({
+            message: "You are signed up"
+        })
+    }
+
 })
 
 userRouter.post("/signin", function (req, res) {
