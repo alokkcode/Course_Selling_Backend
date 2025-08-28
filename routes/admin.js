4081a08afba6481d1a3850629ebd19342e6d17cd
alokkcode <alokkumar.code@gmail.com>
2025-08-28 20:43:30 +0530

    Added admin singup and signin routes

--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,120 @@
 const { Router } = require("express");
 const adminRouter = Router();
-const { adminModel} = require("../db");
+const { z } = require("zod");
+const { adminModel } = require("../db");
+const { JWT_ADMIN_SECRET } = require("../config");
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken");
 
-adminRouter.post("/signup", function (req, res) {
-    res.json({
-        message: "Signup endpoint"
+adminRouter.post("/signup",async function (req, res) {
+    const requiredBody = z.object({ 
+        email: z.string().min(3).max(50).email(),
+        password: z.string().min(6).max(100).refine(function (val) {
+            return /[A-Z]/.test(val);
+        }, {
+            error: "Password must contain at least one uppercase character"
+        }).refine(function (val) {
+            return /[a-z]/.test(val);
+        }, {
+            error: "Password must contain at least one lowercase character"
+        }).refine(function (val) {
+            return /[^a-zA-Z0-9]/.test(val);
+        }, {
+            error: "Password must contain at least one special character"
+        }),
+
+        firstName: z.string(),
+        lastName: z.string()
     })
+
+    const parsedDataWithSuccess = requiredBody.safeParse(req.body);
+
+    if (!parsedDataWithSuccess.success) {
+        res.json({
+            message: "Incorrect Format",
+            error: parsedDataWithSuccess.error
+        })
+        return
+    }
+
+    const { email, password, firstName, lastName } = req.body;
+    let errorThrown = false;
+
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        await adminModel.create({
+            email: email,
+            password: hashedPassword,
+            firstName: firstName,
+            lastName: lastName
+        })
+    } catch (e) {
+        res.json({
+            message: "User already exists"
+        })
+
+        errorThrown = true;
+    }
+
+    if (!errorThrown) {
+        res.json({
+            message: "You are signed up"
+        })
+    }
 })
 
-adminRouter.post("/signin", function (req, res) {
-    res.json({
-        message: "Signin endpoint"
-    })
+adminRouter.post("/signin",async function (req, res) {
+    try {
+        const requiredBody = z.object({
+            email: z.string().min(3).max(50).email(),
+            password: z.string().min(6)
+        })
+
+        const parsedDataWithSuccess = requiredBody.safeParse(req.body);
+
+        if (!parsedDataWithSuccess.success) {
+            res.status(400).json({
+                message: "Incorrect Format",
+                error: parsedDataWithSuccess.error
+            })
+            return
+        }
+
+        const { email, password } = req.body;
+
+        const admin = await adminModel.findOne({
+            email: email
+        })
+
+        if (!admin) {
+            res.status(403).json({
+                message: "Admin does not exist "
+            })
+        }
+
+        const passwordMatch = await bcrypt.compare(password, admin.password);
+        if (passwordMatch) {
+            const token = jwt.sign({
+                id: admin._id.toString()
+            }, JWT_ADMIN_SECRET);
+
+            res.json({
+                token: token
+            })
+
+        } else {
+            res.status(403).json({
+                message: "Incorrect credentials"
+            })
+        }
+
+    } catch (err) {
+        console.log("Signin error:", err);
+        res.status(500).json({
+            message: "Internal Server Error"
+        })
+    }
 })
 
 
